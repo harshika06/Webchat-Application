15b598e1b7e72ceb94dea26996bb9843f8afe43f
Harshika <harshikasaxena2206@gmail.com>
2023-04-15 02:55:21 +0530

    first commit

---
+++ public/client.js
@@ -0,0 +1,34 @@
+// const socket = io()
+//         let name;
+//         let textarea = document.querySelector('#textarea')
+//         let messageaArea = document.querySelector('.message__area')
+//         do{
+//             name = prompt('please enter your name : ')
+//         }
+//         while(!name)
+
+
+//         textarea.addEventListener('keyup',(e)=>
+//         {
+//             if(e.key === 'Enter')
+//             {
+//                 sendMessage(e.target.value)
+//             }
+//         })
+
+//         function sendMessage(message)
+//         {
+//             let msg = 
+//             {
+//                 user : name,
+//                 message : message.trim()
+//             }
+
+//             //append
+
+//             appendMessage(msg,'outgoing')
+//             textarea.value = ''
+//             //scrolltobottom()
+
+
+ 
---
+++ public/server.js
@@ -0,0 +1,32 @@
+const { Socket } = require('engine.io')
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+
+const PORT = process.env.PORT || 3001
+
+http.listen(PORT,()=>{
+    console.log(`Listening on port ${PORT}`)
+})
+
+//Route Create
+
+app.use(express.static(__dirname+'/public'))
+
+app.get('/',(req,res)=>
+{
+    res.sendFile(__dirname+'/index.html')
+
+})
+
+//socket
+
+const io = require('socket.io')(http)
+io.on('connection',(socket)=>
+{
+    console.log('connected...')
+    socket.on('message',(msg) =>
+    {
+         socket.broadcast.emit('message',msg)
+    })
+})
